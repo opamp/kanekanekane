72347aa6835ddddacba7f370d696e33e4c94b87b
Masahiro Nagata <opampg@gmail.com>
2019-04-15 13:38:09 +0900

    range-selector of top page has been shelved. Add new entry button and modal is implemented.

--- static/js/top.js
+++ static/js/top.js
@@ -1,25 +1,12 @@
-/*
-function setup_init_date(){
+function setup_init_addmodal_date(){
     var today = new Date();
     var today_year = today.getFullYear();
     var today_month = ("0"+(today.getMonth()+1)).slice(-2);
     var today_day = ("0"+today.getDate()).slice(-2);
-    document.getElementById("end-date").value = today_year + '-' + today_month + '-' + today_day;
-    document.getElementById("end-date").max = today_year + '-' + today_month + '-' + today_day;
-
-    var onemonthbefore = new Date();
-    onemonthbefore.setMonth(onemonthbefore.getMonth()-1);
-    var onemonthbefore_year = onemonthbefore.getFullYear();
-    var onemonthbefore_month = ("0"+(onemonthbefore.getMonth()+1)).slice(-2);
-    var onemonthbefore_day = ("0"+onemonthbefore.getDate()).slice(-2);
-    document.getElementById("start-date").value = onemonthbefore_year + '-' + onemonthbefore_month + '-' + onemonthbefore_day;
-    set_max_of_start();
-};
-
-function set_max_of_start(){
-    document.getElementById("start-date").max = document.getElementById("end-date").value;
+    document.getElementById("date-of-data").value = today_year + '-' + today_month + '-' + today_day;
+    document.getElementById("date-of-data").max = today_year + '-' + today_month + '-' + today_day;
 }
-*/
+
 window.onload = function() {
-    //setup_init_date();
+    setup_init_addmodal_date();
 };
