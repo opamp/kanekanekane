88fba25a20be23f647040274e4f032f418f2f92d
Masahiro Nagata <opampg@gmail.com>
2019-04-15 14:01:45 +0900

    database table was changed.

--- sql/add-testuser.sql
+++ sql/add-testuser.sql
@@ -1,2 +1,2 @@
 /* username: guest, password: guest*/
-INSERT INTO users (username, password) VALUES ('guest', 'b0e0ec7fa0a89577c9341c16cff870789221b310a02cc465f464789407f83f377a87a97d635cac2666147a8fb5fd27d56dea3d4ceba1fc7d02f422dda6794e3c');
+INSERT INTO users (username, password, balance) VALUES ('guest', 'b0e0ec7fa0a89577c9341c16cff870789221b310a02cc465f464789407f83f377a87a97d635cac2666147a8fb5fd27d56dea3d4ceba1fc7d02f422dda6794e3c', 0);
