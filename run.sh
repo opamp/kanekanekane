06b2eb1df77c7cbfda557b39203e2cdef0a2284c
Masahiro Nagata <opampg@gmail.com>
2019-04-20 21:38:28 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# run.sh is a simple script to run this app for development
+# 1. preparation
+#     1. $ ros install t-sin/ros-tap
+#     2. $ ros tap tap kanekanekane.asd
+# 2. run this script
+#     1. $ ./run.sh
+
 APP_ENV=development clackup --server :woo --port 5000 app.lisp
