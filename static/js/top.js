b17fcca80dda67f315f3e75879871479d1b83d89
Masahiro Nagata <opampg@gmail.com>
2019-04-20 14:54:51 +0900

    Report validity of required input of top.html form was implemented.

--- static/js/top.js
+++ static/js/top.js
@@ -11,47 +11,50 @@ window.onload = function() {
     setup_init_addmodal_date();
 
     $("button#add").click(function() {
-        // disable button when sending data
-        var thisbutton = $(this);
-        thisbutton.attr("disabled",true);
+        if($("#add-input-form").get(0).reportValidity() == true){
+            // disable button when sending data
+            var thisbutton = $(this);
+            thisbutton.attr("disabled",true);
 
-        // read values from input/select
-        var dataname = $("#money-data-name").val();
-        var date = $("#date-of-data").val();
-        var typeisincome = false;
-        if($("#type-of-input").val() != "outlay"){
-            typeisincome = true;
-        }
-        var amount = $("#amount-of-money").val();
-        var cate = $("#cate-input").val();
-        var comment = $("#comment-input").val();
-
-        // build object to send data
-        var data = {
-            name: dataname,
-            date: date,
-            incometype: typeisincome,
-            amount: amount,
-            category: cate,
-            comment: comment
-        };
-        console.log(data); // for checking
-
-        $.ajax({
-            type: "post",
-            url: "/book/write",
-            data: JSON.stringify(data),
-            contentType: 'application/json',
-            dataType: "json",
-            success: function(jsondata){
-                console.log("success pushing");
-            },
-            error: function(){
-                console.log("send error");
-            },
-            complete: function(){
-                thisbutton.attr("disabled",false);
+            // read values from input/select
+            var dataname = $("#money-data-name").val();
+            var date = $("#date-of-data").val();
+            var typeisincome = false;
+            if($("#type-of-input").val() != "outlay"){
+                typeisincome = true;
             }
-        });
+            var amount = $("#amount-of-money").val();
+            var cate = $("#cate-input").val();
+            var comment = $("#comment-input").val();
+
+            // build object to send data
+            var data = {
+                name: dataname,
+                date: date,
+                incometype: typeisincome,
+                amount: amount,
+                category: cate,
+                comment: comment
+            };
+
+            $.ajax({
+                type: "post",
+                url: "/book/write",
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                dataType: "json",
+                success: function(jsondata){
+                    console.log("success pushing");
+                },
+                error: function(){
+                    console.log("send error");
+                },
+                complete: function(){
+                    thisbutton.attr("disabled",false);
+                }
+            });
+        }else{
+            console.log("Required form is not filled out.");
+        }
     });
 };
