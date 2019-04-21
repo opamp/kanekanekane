be5e1069009dd69b5adcb76d04a214d353f75894
Masahiro Nagata <opampg@gmail.com>
2019-04-21 20:44:14 +0900

    read-items and read-data were fixed.

--- src/db/book.lisp
+++ src/db/book.lisp
@@ -19,36 +19,20 @@
                                 :comment comment
                                 :cate_id cate-id)))))
 
-;; (defun read-items (from to username)
-;;   (let ((where-lst `(:and (:= :username ,username))))
-;;     (when from
-;;       (let* ((year (first from))
-;;              (month (second from))
-;;              (day (third from))
-;;              (from-y-cond (when year `(:>= :t_year ,year)))
-;;              (from-m-cond (when month `(:>= :t_month ,month)))
-;;              (from-d-cond (when day `(:>= :t_day ,day))))
-;;         (when from-y-cond (setf where-lst (append where-lst (list from-y-cond))))
-;;         (when from-m-cond (setf where-lst (append where-lst (list from-m-cond))))
-;;         (when from-d-cond (setf where-lst (append where-lst (list from-d-cond))))))
-;;     (when to
-;;       (let* ((year (first to))
-;;              (month (second to))
-;;              (day (third to))
-;;              (from-y-cond (when year `(:<= :t_year ,year)))
-;;              (from-m-cond (when month `(:<= :t_month ,month)))
-;;              (from-d-cond (when day `(:<= :t_day ,day))))
-;;         (when from-y-cond (setf where-lst (append where-lst (list from-y-cond))))
-;;         (when from-m-cond (setf where-lst (append where-lst (list from-m-cond))))
-;;         (when from-d-cond (setf where-lst (append where-lst (list from-d-cond))))))
-;;     (format t "cond -> ~A~%" where-lst)
-;;     (with-connection (db)
-;;       (if (= (length where-lst) 2)
-;;           (retrieve-all (select :*
-;;                                 (from :book)
-;;                                 (inner-join :categories :on (:= :book.cate_id :categories.id))
-;;                                 (where (:= :username username))))
-;;           (retrieve-all (select :*
-;;                                 (from :book)
-;;                                 (inner-join :categories :on (:= :book.cate_id :categories.id))
-;;                                 (where where-lst)))))))
+(defun read-items (from to username)
+  (let ((where-lst `(:and (:= :username ,username))))
+    (when from
+      (setf where-lst (append where-lst `((:>= :record_date ,from)))))
+    (when to
+      (setf where-lst (append where-lst `((:<= :record_date ,to)))))
+    (with-connection (db)
+      (format t "~A~%" where-lst)
+      (if (= (length where-lst) 2)
+          (retrieve-all (select :*
+                                (from :book)
+                                (inner-join :categories :on (:= :book.cate_id :categories.id))
+                                (where (:= :username username))))
+          (retrieve-all (select :*
+                                (from :book)
+                                (inner-join :categories :on (:= :book.cate_id :categories.id))
+                                (where where-lst)))))))
